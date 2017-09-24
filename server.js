825e764e2be65f0976eed2b446f8fafc48eb441f
gonzalo <gonzalo.abruna@gmail.com>
2017-09-24 12:01:06 +0200

    modified:   server.js

--- server.js
+++ server.js
@@ -166,7 +166,7 @@ app.put('/api/routine-item/:id', (req, res) => {
 
 // Serve the built angular app
 app.get('*', (req, res) => {
-    res.sendfile('./dist/index.html');
+    res.sendfile(`${__dirname}/dist/index.html`);
 });
 
 // Start the server:
